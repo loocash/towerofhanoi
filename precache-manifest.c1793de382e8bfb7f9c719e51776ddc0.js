53da249d29168f767f3db1a291e7c3de355dbde3
loocash <lukaszkapica@gmail.com>
2019-05-11 23:32:28 +0200

    Make gh-page

---
+++ precache-manifest.c1793de382e8bfb7f9c719e51776ddc0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1e2164ee8b1dbf49409de6dc0e66b967",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1fbdc68bb8e1d6597bda",
+    "url": "/static/css/2.4223df75.chunk.css"
+  },
+  {
+    "revision": "37732f60b19e3f8ffe5b",
+    "url": "/static/css/main.ff59c320.chunk.css"
+  },
+  {
+    "revision": "1fbdc68bb8e1d6597bda",
+    "url": "/static/js/2.bfae57b7.chunk.js"
+  },
+  {
+    "revision": "37732f60b19e3f8ffe5b",
+    "url": "/static/js/main.b44bc424.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);

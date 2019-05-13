f82b2b3ce86bc2d936b4d4a8adba7d173e197456
loocash <lukaszkapica@gmail.com>
2019-05-13 13:50:35 +0200

    Updates

---
+++ precache-manifest.466c58ff88b7a80a1499eb36c3b080c1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8be116508a67d76c2a8eceacbc0414b7",
+    "url": "/towerofhanoi/index.html"
+  },
+  {
+    "revision": "1fbdc68bb8e1d6597bda",
+    "url": "/towerofhanoi/static/css/2.4223df75.chunk.css"
+  },
+  {
+    "revision": "e78858935f5c6078a89a",
+    "url": "/towerofhanoi/static/css/main.ff59c320.chunk.css"
+  },
+  {
+    "revision": "1fbdc68bb8e1d6597bda",
+    "url": "/towerofhanoi/static/js/2.bfae57b7.chunk.js"
+  },
+  {
+    "revision": "e78858935f5c6078a89a",
+    "url": "/towerofhanoi/static/js/main.6372c483.chunk.js"
+  },
+  {
+    "revision": "5e0bea053b44f045649f",
+    "url": "/towerofhanoi/static/js/runtime~main.deb3d622.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.c1793de382e8bfb7f9c719e51776ddc0.js"
+  "/towerofhanoi/precache-manifest.466c58ff88b7a80a1499eb36c3b080c1.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/towerofhanoi/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
